c804e46887fe8d4221c03f1cd2926641a8b60f8b
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2022-02-03 14:58:04 -0700

    Fixed layout and moved layout code to the setupLayout method.

--- src/gui/view/MorningPanel.java
+++ src/gui/view/MorningPanel.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -37,15 +38,7 @@ public class MorningPanel extends JPanel
 		this.duckLabel = new JLabel("Duck status here");
 		
 		this.layout = new SpringLayout();
-		layout.putConstraint(SpringLayout.NORTH, duckLabel, 31, SpringLayout.SOUTH, duckButton);
-		layout.putConstraint(SpringLayout.WEST, duckLabel, 0, SpringLayout.WEST, demoLabel);
-		layout.putConstraint(SpringLayout.NORTH, duckButton, 58, SpringLayout.NORTH, this);
-		layout.putConstraint(SpringLayout.EAST, duckButton, 0, SpringLayout.EAST, demoButton);
-		layout.putConstraint(SpringLayout.NORTH, demoLabel, 24, SpringLayout.SOUTH, demoButton);
-		layout.putConstraint(SpringLayout.EAST, demoLabel, -167, SpringLayout.EAST, this);
-		layout.putConstraint(SpringLayout.SOUTH, demoButton, -100, SpringLayout.SOUTH, this);
-		layout.putConstraint(SpringLayout.EAST, demoButton, -141, SpringLayout.EAST, this);
-
+		
 		setupPanel();
 		setupListeners();
 		setupLayout();
@@ -56,6 +49,7 @@ public class MorningPanel extends JPanel
 	{
 		this.setLayout(layout);
 		this.setBackground(new Color(132, 3, 45));
+		this.setPreferredSize(new Dimension(800,600));
 		this.add(demoButton);
 		this.add(demoLabel);
 		this.add(duckButton);
@@ -86,6 +80,18 @@ public class MorningPanel extends JPanel
 
 	private void setupLayout()
 	{
+		layout.putConstraint(SpringLayout.WEST, demoButton, 0, SpringLayout.WEST, duckButton);
+		layout.putConstraint(SpringLayout.EAST, demoButton, 0, SpringLayout.EAST, duckButton);
+		layout.putConstraint(SpringLayout.WEST, duckButton, 50, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, duckButton, -50, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.WEST, demoLabel, 0, SpringLayout.WEST, duckLabel);
+		layout.putConstraint(SpringLayout.WEST, duckLabel, 20, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, duckLabel, -20, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.NORTH, demoLabel, 62, SpringLayout.SOUTH, demoButton);
+		layout.putConstraint(SpringLayout.EAST, demoLabel, 0, SpringLayout.EAST, duckLabel);
+		layout.putConstraint(SpringLayout.NORTH, demoButton, 72, SpringLayout.SOUTH, duckLabel);
+		layout.putConstraint(SpringLayout.NORTH, duckLabel, 46, SpringLayout.SOUTH, duckButton);
+		layout.putConstraint(SpringLayout.NORTH, duckButton, 185, SpringLayout.NORTH, this);
 
 	}
 
